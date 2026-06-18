b530dec749bacf89e18a67f925b02a8f1324ff90
Pui Yong Qing <puiyongqing@gmail.com>
2026-06-19 00:17:25 +0800

    nix: migrate flake.nix to flake-parts; add treefmt, multi-arch

--- flake.nix
+++ flake.nix
@@ -1,24 +1,55 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
-    inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${pkgs.stdenv.hostPlatform.system}.default = pkgs.callPackage ./package.nix { };
-      devShells.${pkgs.stdenv.hostPlatform.system}.default =
-        pkgs.mkShell.override { stdenv = pkgs.clangStdenv; }
-          {
+    inputs@{ self, ... }:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      imports = [ inputs.treefmt-nix.flakeModule ];
+
+      perSystem =
+        { pkgs, self', ... }:
+        {
+          packages = {
+            "run0-pkexec-shim" = pkgs.callPackage ./package.nix { };
+            default = self'.packages."run0-pkexec-shim";
+          };
+
+          devShells.default = pkgs.mkShell.override { stdenv = pkgs.clangStdenv; } {
+            inputsFrom = [ self'.packages.default ];
             packages = with pkgs; [
-              cargo
-              rustc
-              rustfmt
-              clippy
               rust-analyzer
             ];
           };
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs = {
+              deadnix.enable = true;
+              nixfmt = {
+                enable = true;
+                package = pkgs.nixfmt-rs;
+              };
+              rustfmt.enable = true;
+              statix.enable = true;
+              taplo.enable = true;
+            };
+          };
+        };
+
+      };
     };
 }
